0d12db03fb35d78c7fc0de3fc19f235503657fec
home <home@home-PC>
2018-09-22 10:20:59 +0530

    added changes

--- GITRepo/src/testcase1.java
+++ GITRepo/src/testcase1.java
@@ -6,7 +6,7 @@ public class testcase1 {
 		System.out.println("Testcase 1 ");
 		System.out.println("Testcase 1 is passed ");
 		System.out.println("ok it is passed now ");
-		System.out.println("ok ok  ");
+	
 		
 		System.out.println("Continuous Integration Passed ");
 		
