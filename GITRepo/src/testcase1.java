b6a26531c19d99669b1ba86c6a4bee705eac6a17
home <home@home-PC>
2018-09-23 09:48:04 +0530

    slsdlsdssds' sds dsdsd sds d s

--- GITRepo/src/testcase1.java
+++ GITRepo/src/testcase1.java
@@ -3,10 +3,6 @@ public class testcase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testcase 1 ");
-		System.out.println("Testcase 1 is passed ");
-		System.out.println("ok it is passed now ");
-		System.out.println("ok ok  ");
 		
 		System.out.println("Continuous Integration Passed ");
 		
