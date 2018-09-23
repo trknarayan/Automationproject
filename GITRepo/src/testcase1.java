67a83b1a55d8483e68594aca8c18b3cbc987c475
home <home@home-PC>
2018-09-23 09:05:52 +0530

    add

--- GITRepo/src/testcase1.java
+++ GITRepo/src/testcase1.java
@@ -4,7 +4,7 @@ public class testcase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testcase 1 ");
-		System.out.println("Testcase 1 is passed ");
+		//System.out.println("Testcase 1 is passed ");
 		
 		System.out.println("Testcase 1 is passed ");
 		System.out.println("Testcase 2 is passed ");
